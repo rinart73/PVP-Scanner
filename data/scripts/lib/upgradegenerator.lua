8f2a87e1a150ea67f2464a2da92f88892ce14455
Artem Romanko <rinart73@gmail.com>
2019-07-21 20:37:45 +0200

    Fixed an error

--- data/scripts/lib/upgradegenerator.lua
+++ data/scripts/lib/upgradegenerator.lua
@@ -1,7 +1,7 @@
 if onServer() then
     local Azimuth = include("azimuthlib-basic")
     local PVPScannerConfig = Azimuth.loadConfig("PVPScanner", {
-      "UpgradeWeight" = {default = 0.5, min = 0, max = 1000, comment = "Relative chance of getting this upgrade from 0.0 to 1000."}
+      ["UpgradeWeight"] = {default = 0.5, min = 0, max = 1000, comment = "Relative chance of getting this upgrade from 0.0 to 1000."}
     })
     UpgradeGenerator.add("data/scripts/systems/pvpscanner.lua", PVPScannerConfig.UpgradeWeight)
 else -- just in case
